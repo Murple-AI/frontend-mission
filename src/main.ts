0dc325de549971e13f5044eef3aab4a074ae1193
Jaegeon <101312797+jaegeon-murple@users.noreply.github.com>
2024-12-13 14:39:33 +0900

    초기 개발환경 설정 (#1)
    * pnpm create vite app vue-ts
    
    * setup tailwind css
    
    * setup eslint
    
    * setup prettier
    
    과제 셋업 (#2)
    
    * 경로 설정
    
    * 과제용 컴포넌트 구현
    
    README 내용 추가
    
    Update README.md
    
    Update README.md
    
    보더 디자인 추가, README 정리
    
    Update README.md
    
    Update README.md
    
    인풋 보더 내부 x 버튼 추가
    
    eslint prettier / tailwind
    
    필드 유효성검사 / composable패턴으로 반응형 적용
    
    eslint --fix / 마무리
    
    init
    
    chore: submit 경로 변경
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    수정
    
    tailwind 적용
    
    update copy.yaml
    
    update copy.yaml
    
    update copy.yaml
    
    Update copy.yaml
    
    chore: meaningless commit 1
    
    chore: meaningless commit 2
    
    chore: meaningless commit 3
    
    chore: meaningless commit 4
    
    chore: meaningless commit 5

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import { VueQueryPlugin, QueryClient } from "@tanstack/vue-query";
+import App from "./App.vue";
+import "./style.css";
+
+async function prepareApp() {
+  const { worker } = await import("./mocks/browser");
+  return worker.start();
+}
+const queryClient = new QueryClient();
+const app = createApp(App);
+
+prepareApp().then(() => {
+  app.use(VueQueryPlugin, { queryClient });
+  app.mount("#app");
+});
